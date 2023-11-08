5bcc620668e74668003ca3a2b8650aaac349219f
noah305 <nourahmad305@hotmail.com>
2023-11-08 23:34:29 +0200

    Coded finished tasks

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ let input_priority = document.getElementById("priority")
 let add_btn = document.getElementById("add-task-btn");
 let tasks_list = document.getElementsByClassName('tasks-list')[0]
 
+// let all_tasks=[];
+let ongoing_tasks=[];
+let finished_tasks=[];
+
 
 add_btn.addEventListener('click', add_task_fct);
 let counter_nb_of_tasks= 0
@@ -11,6 +15,10 @@ let tasks_list_leng= document.getElementsByClassName('tasks-list').length
 
 console.log("test list lenght")
 console.log( tasks_list_leng)
+
+
+
+
 // function to add task
 function add_task_fct() {
 
@@ -25,8 +33,11 @@ function add_task_fct() {
         return false
     }
 
+
+
     // high priority
     if (input_priority.value == "high") {
+
         counter_nb_of_tasks += 1
 
         console.log("high test")
@@ -48,6 +59,7 @@ function add_task_fct() {
     // low priority
     else {
 
+       
         counter_nb_of_tasks += 1
 
         console.log("low test")
@@ -66,32 +78,49 @@ function add_task_fct() {
         tasks_list.appendChild(new_task_added)
     }
 
+
+    //add task all_tasks list and ongoing tasks lists
+    // all_tasks.push(input_task.value);
+    // console.log('all tasks', all_tasks)
+    ongoing_tasks.push(input_task.value);
+    console.log('ongoing tasks', ongoing_tasks)
+
     //clear input field
     input_task.value = ""
 
 
 
-    //function to remove task
-    // let delete_btn = document.getElementById(`delete-btn-${counter_nb_of_tasks}`)
+
     let task_index= counter_nb_of_tasks-1
+    let current_task_tile = document.getElementsByClassName('task-title')[task_index]
+
+    //function to remove task
     console.log("index",task_index)
     let delete_btn = document.getElementsByClassName('delete-btn')[task_index]
     console.log("test",delete_btn)
-    console.log("test id",document.getElementById(`delete-btn-${counter_nb_of_tasks}`))
-
 
     delete_btn.addEventListener('click', function (e) {
+
+       
+        
+        let removed_task= ongoing_tasks.splice(task_index,1)
+        console.log("deleted from all tasks",ongoing_tasks,removed_task[0])
+
         console.log("trying to delete")
-        e.target.closest('li').remove();
+         e.target.closest('li').remove();
+        counter_nb_of_tasks-=1;
+
+
+
     })
 
 
     // function to edit task title
-    // let edit_btn = document.getElementById(`edit-btn-${counter_nb_of_tasks}`)
     let edit_btn = document.getElementsByClassName('edit-btn')[task_index]
-    let current_priority = edit_btn.previousElementSibling;
-    let current_due_date = current_priority.previousElementSibling;
-    let current_task_tile = current_due_date.previousElementSibling;
+    // let current_task_tile = document.getElementsByClassName('task-title')[task_index]
+    // let current_priority = edit_btn.previousElementSibling;
+    // let current_due_date = current_priority.previousElementSibling;
+    // let current_task_tile = current_due_date.previousElementSibling;
     console.log(current_task_tile)
 
     edit_btn.addEventListener('click', function (e) {
@@ -103,17 +132,36 @@ function add_task_fct() {
 
 
     // function to change task status
-    // let status_btn=document.getElementById(`status-button-${counter_nb_of_tasks}`)
     let status_btn=document.getElementsByClassName('status-button')[task_index]
     console.log(status_btn)
+
     status_btn.addEventListener('click', function (e) {
         console.log("trying to change status")
+
         status_btn.classList.toggle("finished-status")
         if(status_btn.innerHTML=="Ongoing"){
+
+            console.log(task_index)
+
             status_btn.innerHTML="Finished"
+            // let done_task= ongoing_tasks.splice(task_index,1)
+            // // console.log("removed from ongoing",ongoing_tasks,done_task[0])
+
+            let done_task=current_task_tile.innerText
+            finished_tasks.push(done_task)
+            console.log("added to finished",finished_tasks)
+            
+
+
 
         }
         else{
+
+            let re_do_task=current_task_tile.innerText
+            const index = finished_tasks.indexOf(re_do_task); 
+            finished_tasks.splice(index,1)
+
+            console.log("added to finished",finished_tasks)
             status_btn.innerHTML="Ongoing"
 
         }
